62ca56751464d2f8e3e8a270c7237359cb2a2ef4
DKAT-DAVI <eudkatdavikalel@gmail.com>
2023-03-08 23:05:11 -0300

    Add yup dependency to data validation

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -1,12 +1,30 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
 interface ICidade {
-  nome: string;
+  name: string;
 }
 
-export const create = (request: Request<{}, {}, ICidade>, response: Response) => {
-  
-  console.log(request.body.nome);
+const bodyValitadtion: yup.Schema<ICidade> = yup.object().shape({
+  name: yup.string().required().min(3),
+});
 
+export const create = async (request: Request<{}, {}, ICidade>, response: Response) => {
+  let validatedData: ICidade | undefined = undefined;
+
+  try {
+    validatedData = await bodyValitadtion.validate(request.body)
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    return response.json({
+      error: {
+        default: yupError.message,
+      }
+    })
+  }
+
+  console.log(validatedData);
   return response.send('City created!');
 }
